b210862c9955683cc3f7bc7e7ad8f9db28d9502e
Matias Savela <matias.savela@codemate.com>
2020-09-28 16:02:00 +0300

    Fetch Task logs based on attempt_id.

--- services/ui_backend_service/api/log.py
+++ services/ui_backend_service/api/log.py
@@ -31,6 +31,8 @@ class LogApi(object):
 
     @handle_exceptions
     async def get_task_log_stdout(self, request):
+        attempt_id = request.query.get("attempt_id", None)
+
         bucket, path, _ = \
             await get_metadata_log(
                 self._async_table.find_records,
@@ -38,16 +40,22 @@ class LogApi(object):
                 request.match_info.get("run_number"),
                 request.match_info.get("step_name"),
                 request.match_info.get("task_id"),
+                attempt_id,
                 STDOUT)
 
         if bucket and path:
-            lines = await read_and_output(bucket, path)
-            return web_response(200, {'data': lines})
+            try:
+                lines = await read_and_output(bucket, path)
+                return web_response(200, {'data': lines})
+            except Exception:
+                return web_response(502, {'data': []})
         else:
-            return web_response(200, {'data': []})
+            return web_response(404, {'data': []})
 
     @handle_exceptions
     async def get_task_log_stderr(self, request):
+        attempt_id = request.query.get("attempt_id", None)
+
         bucket, path, _ = \
             await get_metadata_log(
                 self._async_table.find_records,
@@ -55,37 +63,54 @@ class LogApi(object):
                 request.match_info.get("run_number"),
                 request.match_info.get("step_name"),
                 request.match_info.get("task_id"),
+                attempt_id,
                 STDERR)
 
         if bucket and path:
-            lines = await read_and_output(bucket, path)
-            return web_response(200, {'data': lines})
+            try:
+                lines = await read_and_output(bucket, path)
+                return web_response(200, {'data': lines})
+            except Exception:
+                return web_response(502, {'data': []})
         else:
-            return web_response(200, {'data': []})
+            return web_response(404, {'data': []})
 
 
-async def get_metadata_log(find_records, flow_name, run_number, step_name, task_id, field_name) -> (str, str, int):
+async def get_metadata_log(find_records, flow_name, run_number, step_name, task_id, attempt_id, field_name) -> (str, str, int):
     run_id_key, run_id_value = translate_run_key(run_number)
     task_id_key, task_id_value = translate_task_key(task_id)
 
     try:
-        result, _ = await find_records(
+        results, _ = await find_records(
             conditions=["flow_id = %s",
                         "{run_id_key} = %s".format(run_id_key=run_id_key),
                         "step_name = %s",
                         "{task_id_key} = %s".format(task_id_key=task_id_key),
                         "field_name = %s"],
-            values=[flow_name, run_id_value, step_name, task_id_value, field_name], limit=1, offset=0,
-            order=["ts_epoch DESC"], groups=None, fetch_single=True, enable_joins=True
+            values=[flow_name, run_id_value, step_name, task_id_value, field_name], limit=0, offset=0,
+            order=["ts_epoch DESC"], groups=None, fetch_single=False, enable_joins=True
         )
-        if result.response_code == 200:
-            value = json.loads(result.body['value'])
-            if value['ds_type'] == 's3':
-                url = urlparse(value['location'], allow_fragments=False)
-                bucket = url.netloc
-                path = url.path.lstrip('/')
-                attempt_id = value['attempt']
-                return bucket, path, attempt_id
+        if results.response_code == 200:
+            if attempt_id is None and len(results.body) > 0:
+                result = results.body[0]
+            else:
+                for r in results.body:
+                    try:
+                        value = json.loads(r['value'])
+                        if value['attempt'] == int(attempt_id):
+                            result = r
+                            break
+                    except Exception:
+                        pass
+
+            if result:
+                value = json.loads(result['value'])
+                if value['ds_type'] == 's3':
+                    url = urlparse(value['location'], allow_fragments=False)
+                    bucket = url.netloc
+                    path = url.path.lstrip('/')
+                    attempt_id = value['attempt']
+                    return bucket, path, attempt_id
     except Exception:
         pass
     return None, None, None
