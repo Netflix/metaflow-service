ef72f9a279a45766ed9a9374a1be410086641f5b
Sakari Ikonen <sakari.ikonen@codemate.com>
2020-10-06 16:04:47 +0300

    added task heartbeat monitor. working for task subs, but does not handle attempts yet.

--- services/ui_backend_service/api/heartbeat_monitor.py
+++ services/ui_backend_service/api/heartbeat_monitor.py
@@ -10,7 +10,7 @@ HEARTBEAT_INTERVAL = 10 # interval of heartbeats, in seconds
 class HeartbeatMonitor(object):
   def __init__(self, event_name, event_emitter=None):
     self.watched = {}
-    # Handle DB Events
+    # Handle HB Events
     self.event_emitter = event_emitter or AsyncIOEventEmitter()
     event_emitter.on(event_name, self.heartbeat_handler)
 
@@ -26,9 +26,9 @@ class HeartbeatMonitor(object):
     "Adds object to heartbeat monitoring"
     raise NotImplementedError
 
-  async def remove_from_watch(self, key):
+  def remove_from_watch(self, key):
     "Removes object from heartbeat monitoring"
-    raise NotImplementedError
+    self.watched.pop(key, None)
 
   async def load_and_broadcast(self, key):
     '''Triggered when a heartbeat for a key has expired.
@@ -42,7 +42,6 @@ class HeartbeatMonitor(object):
     '''
     while True:
       time_now = int(datetime.datetime.utcnow().timestamp()) # same format as the metadata heartbeat uses
-      # print(f"checking heartbeats of {len(self.watched)} runs, timestamp: {time_now}", flush=True)
       for key, hb in list(self.watched.items()):
         if time_now - hb > HEARTBEAT_INTERVAL * 2:
           await self.load_and_broadcast(key)
@@ -87,9 +86,6 @@ class RunHeartbeatMonitor(HeartbeatMonitor):
       if heartbeat_ts is not None: # only start monitoring on runs that have a heartbeat
         self.watched[run_number] = heartbeat_ts
 
-  def remove_from_watch(self, run_id):
-    self.watched.pop(run_id, None)
-
   async def get_run(self, run_id):
     # Remember to enable_joins for the query, otherwise the 'status' will be missing from the run
     # and we can not broadcast an up-to-date status.
@@ -101,8 +97,83 @@ class RunHeartbeatMonitor(HeartbeatMonitor):
                           )
     return result.body if result.response_code==200 else None
   
-  async def load_and_broadcast(self, run_id):
-    run = await self.get_run(run_id)
+  async def load_and_broadcast(self, key):
+    run = await self.get_run(key)
     resources = resource_list(self._run_table.table_name, run)
     self.event_emitter.emit('notify', 'UPDATE', resources, run)
 
+class TaskHeartbeatMonitor(HeartbeatMonitor):
+  '''
+  Service class for adding objects with heartbeat timestamps to be monitored and acted upon
+  when heartbeat becomes too old.
+
+  Starts an async watcher loop that periodically checks the heartbeat of the subscribed tasks for expired timestamps
+
+  Usage
+  -----
+  Responds to event_emitter emissions with messages:
+    "task-heartbeat", "update", data -> updates heartbeat timestamp that is found in database
+    "task-heartbeat", "complete" data -> removes task from heartbeat checks
+  '''
+  def __init__(self, event_emitter=None):
+    # Init the abstract class
+    super().__init__(
+      event_name="task-heartbeat",
+      event_emitter=event_emitter
+    )
+    # Table for data fetching for load_and_broadcast and add_to_watch
+    self._task_table = AsyncPostgresDB.get_instance().task_table_postgres
+
+  async def heartbeat_handler(self, action, data):
+    if action == "update":
+      await self.add_to_watch(data)
+    elif action == "complete":
+      key = self.generate_hashmap_key(data)
+      self.remove_from_watch(key)
+    
+  async def add_to_watch(self, data):
+    task = await self.get_task(
+      data["flow_id"],
+      data["run_number"],
+      data["step_name"],
+      data["task_id"]
+    )
+
+    key = self.generate_hashmap_key(data)
+    if key and "last_heartbeat_ts" in task:
+      heartbeat_ts = task["last_heartbeat_ts"]
+      if heartbeat_ts is not None: # only start monitoring on runs that have a heartbeat
+        self.watched[key] = heartbeat_ts
+
+  async def get_task(self, flow_id, run_number, step_name, task_id):
+    # Remember to enable_joins for the query, otherwise the 'status' will be missing from the run
+    # and we can not broadcast an up-to-date status.
+    result, _ = await self._task_table.find_records(
+                            conditions=[
+                              "flow_id = %s",
+                              "run_number = %s",
+                              "step_name = %s",
+                              "task_id = %s"
+                            ],
+                            values=[flow_id, run_number, step_name, task_id],
+                            fetch_single=True,
+                            enable_joins=True
+                          )
+    return result.body if result.response_code==200 else None
+  
+  async def load_and_broadcast(self, key):
+    flow_id, run_number, step_name, task_id = self.decode_key_ids(key)
+    task = await self.get_task(flow_id, run_number, step_name, task_id)
+    resources = resource_list(self._task_table.table_name, task)
+    self.event_emitter.emit('notify', 'UPDATE', resources, task)
+  
+  def generate_hashmap_key(self, data):
+    "Creates an unique key for the 'watched' dictionary for storing the heartbeat of a specific task"
+    try:
+      return "{flow_id}/{run_number}/{step_name}/{task_id}".format(**data)
+    except:
+      return None
+  
+  def decode_key_ids(self, key):
+    flow, run, step, task = key.split("/")
+    return flow, run, step, task
