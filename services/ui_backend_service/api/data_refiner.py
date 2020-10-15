61d4b01dcfd4532a684832287624e26142b76ec5
Sakari Ikonen <sakari.ikonen@codemate.com>
2020-10-15 14:57:46 +0300

    style fixes and comments.

--- services/ui_backend_service/api/data_refiner.py
+++ services/ui_backend_service/api/data_refiner.py
@@ -2,47 +2,60 @@ from ..cache.store import CacheStore
 from services.data.db_utils import DBResponse
 import json
 
+
 class Refinery(object):
-  "Used to refine objects with data only available from S3"
+    """Used to refine objects with data only available from S3.
 
-  def __init__(self, field_names = []):
-    self.artifact_store = CacheStore().artifact_cache
-    self.field_names = field_names
+    Parameters:
+    -----------
+    field_names: list of field names that contain S3 locations to be replaced with content.
+    """
 
-  async def refine_record(self, record):
-    locations = [ record[field] for field in self.field_names if field in record ]
-    data = await self.fetch_data(locations)
-    _rec = record
-    for k, v in _rec.items():
-      if k in self.field_names and v in data:
-        _rec[k] = data[v]
-    return _rec
+    def __init__(self, field_names=[]):
+        self.artifact_store = CacheStore().artifact_cache
+        self.field_names = field_names
 
-  async def refine_records(self, records):
-    locations = [ record[field] for field in self.field_names for record in records if field in record ]
-    data = await self.fetch_data(locations)
+    async def refine_record(self, record):
+        locations = [record[field] for field in self.field_names if field in record]
+        data = await self.fetch_data(locations)
+        _rec = record
+        for k, v in _rec.items():
+            if k in self.field_names and v in data:
+                _rec[k] = data[v]
+        return _rec
 
-    _recs = []
-    for rec in records:
-      for k, v in rec.items():
-        if k in self.field_names and v in data:
-          rec[k] = data[v]
-      _recs.append(rec)
-    return _recs
-  
-  async def fetch_data(self, locations):
-    _res = await self.artifact_store.cache.GetArtifacts(locations)
-    if not _res.is_ready():
-      await _res.wait()
-    return _res.get()
+    async def refine_records(self, records):
+        locations = [record[field] for field in self.field_names for record in records if field in record]
+        data = await self.fetch_data(locations)
 
-  async def postprocess(self, response: DBResponse):
-    if response.response_code != 200 or not response.body:
-        return response
-    if isinstance(response.body, list):
-      body = await self.refine_records(response.body)
-    else:
-      body = await self.refine_record(response.body)
-    
-    return DBResponse(response_code=response.response_code,
-                      body=body)
+        _recs = []
+        for rec in records:
+            for k, v in rec.items():
+                if k in self.field_names and v in data:
+                    rec[k] = data[v]
+            _recs.append(rec)
+        return _recs
+
+    async def fetch_data(self, locations):
+        _res = await self.artifact_store.cache.GetArtifacts(locations)
+        if not _res.is_ready():
+            await _res.wait()
+        return _res.get()
+
+    async def postprocess(self, response: DBResponse):
+        """
+        Async post processing callback that can be used as the find_records helpers
+        postprocessing parameter.
+
+        Passed in DBResponse will be refined on the configured field_names, replacing the S3 locations
+        with their contents.
+        """
+        if response.response_code != 200 or not response.body:
+            return response
+        if isinstance(response.body, list):
+            body = await self.refine_records(response.body)
+        else:
+            body = await self.refine_record(response.body)
+
+        return DBResponse(response_code=response.response_code,
+                          body=body)
