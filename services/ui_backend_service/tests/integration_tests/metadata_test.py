d84fa82d6f6fba0a028c8132c9be22d389fd69d7
Matias Savela <matias.savela@codemate.com>
2020-10-22 11:37:20 +0300

    Add test metadata primary key changes. More specifically ability to have S3 log files for multiple attempts.
    Migration introduced here:
    https://github.com/Netflix/metaflow-service/pull/30

--- services/ui_backend_service/tests/integration_tests/metadata_test.py
+++ services/ui_backend_service/tests/integration_tests/metadata_test.py
@@ -50,3 +50,43 @@ async def test_list_metadata(cli, db):
 
     await _test_list_resources(cli, db, "/flows/{flow_id}/runs/{run_number}/metadata".format(**_task), 200, [_metadata])
     await _test_list_resources(cli, db, "/flows/{flow_id}/runs/{run_number}/steps/{step_name}/tasks/{task_id}/metadata".format(**_metadata), 200, [_metadata])
+
+
+async def test_list_metadata_field_names(cli, db):
+    _flow = (await add_flow(db, flow_id="HelloFlow")).body
+    _run = (await add_run(db, flow_id=_flow.get("flow_id"))).body
+    _step = (await add_step(db, flow_id=_run.get("flow_id"), step_name="step", run_number=_run.get("run_number"), run_id=_run.get("run_id"))).body
+    _task = (await add_task(db,
+                            flow_id=_step.get("flow_id"),
+                            step_name=_step.get("step_name"),
+                            run_number=_step.get("run_number"),
+                            run_id=_step.get("run_id"))).body
+
+    await _test_list_resources(cli, db, "/flows/{flow_id}/runs/{run_number}/metadata".format(**_task), 200, [])
+    await _test_list_resources(cli, db, "/flows/{flow_id}/runs/{run_number}/steps/{step_name}/tasks/{task_id}/metadata".format(**_task), 200, [])
+
+    _metadata_first = (await add_metadata(db,
+                                          flow_id=_task.get("flow_id"),
+                                          run_number=_task.get("run_number"),
+                                          run_id=_task.get("run_id"),
+                                          step_name=_task.get("step_name"),
+                                          task_id=_task.get("task_id"),
+                                          task_name=_task.get("task_name"),
+                                          metadata={
+                                              "field_name": "log_location_stdout",
+                                              "value": '{"ds_type": "s3", "location": "s3://bucket/Flow/1/end/2/0.stdout.log", "attempt": 0}',
+                                              "type": "log_path"})).body
+
+    _metadata_second = (await add_metadata(db,
+                                           flow_id=_task.get("flow_id"),
+                                           run_number=_task.get("run_number"),
+                                           run_id=_task.get("run_id"),
+                                           step_name=_task.get("step_name"),
+                                           task_id=_task.get("task_id"),
+                                           task_name=_task.get("task_name"),
+                                           metadata={
+                                               "field_name": "log_location_stdout",
+                                               "value": '{"ds_type": "s3", "location": "s3://bucket/Flow/1/end/2/1.stdout.log", "attempt": 1}',
+                                               "type": "log_path"})).body
+
+    await _test_list_resources(cli, db, "/flows/{flow_id}/runs/{run_number}/steps/{step_name}/tasks/{task_id}/metadata".format(**_task), 200, [_metadata_second, _metadata_first])
