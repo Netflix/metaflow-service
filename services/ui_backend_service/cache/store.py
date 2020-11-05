b2aa651fb83e990b3e6bed3e1cd290015c43222d
Sakari Ikonen <sakari.ikonen@codemate.com>
2020-11-05 10:01:49 +0200

    changed data preload not to block server process by using run_coroutine_threadsafe

--- services/ui_backend_service/cache/store.py
+++ services/ui_backend_service/cache/store.py
@@ -1,3 +1,4 @@
+from .generate_dag_action import GenerateDag
 from services.data.postgres_async_db import AsyncPostgresDB
 from services.data.db_utils import translate_run_key
 from pyee import ExecutorEventEmitter
@@ -8,9 +9,11 @@ import asyncio
 import time
 import os
 
+
 class CacheStore(object):
     "Singleton class for all the different cache clients that are used to access caches"
     instance = None
+
     def __init__(self, event_emitter=None):
         if not CacheStore.instance:
             CacheStore.instance = CacheStore.__CacheStore(event_emitter)
@@ -27,12 +30,15 @@ class CacheStore(object):
         async def start_caches(self, app):
             await self.artifact_cache.start_cache()
             await self.dag_cache.start_cache()
-        
+
         async def stop_caches(self, app):
             await self.artifact_cache.stop_cache()
             await self.dag_cache.stop_cache()
 
+
 METAFLOW_ARTIFACT_PREFETCH_RUNS_LIMIT = os.environ.get('PREFETCH_RUNS_LIMIT', 50)
+
+
 class ArtifactCacheStore(object):
     def __init__(self, event_emitter):
         self.event_emitter = event_emitter or ExecutorEventEmitter()
@@ -49,12 +55,12 @@ class ArtifactCacheStore(object):
     async def start_cache(self):
         actions = [SearchArtifacts, GetArtifacts]
         self.cache = CacheAsyncClient('cache_data/artifact_search',
-                                    actions,
-                                    max_size=600000,
-                                    max_actions=32)
+                                      actions,
+                                      max_size=600000,
+                                      max_actions=32)
         await self.cache.start()
-        await self.preload_initial_data()
-    
+        asyncio.run_coroutine_threadsafe(self.preload_initial_data(), self.loop)
+
     async def preload_initial_data(self):
         "Preloads some data on cache startup"
         recent_run_ids = await self.get_recent_run_numbers()
@@ -71,7 +77,7 @@ class ArtifactCacheStore(object):
             print(event, flush=True)
 
     async def get_recent_run_numbers(self):
-        since = int(round(time.time() * 1000)) - 86400_000 * 2 # 2 days ago
+        since = int(round(time.time() * 1000)) - 86400_000 * 2  # 2 days ago
         _records, _ = await self._run_table.find_records(
             conditions=["ts_epoch >= %s"],
             values=[since],
@@ -80,11 +86,11 @@ class ArtifactCacheStore(object):
             expanded=True
         )
 
-        return [ run['run_number'] for run in _records.body ]
-    
-    async def get_artifact_locations_for_run_ids(self, run_ids = []):
+        return [run['run_number'] for run in _records.body]
+
+    async def get_artifact_locations_for_run_ids(self, run_ids=[]):
         # do not touch DB if no run_ids were given.
-        if len(run_ids)==0:
+        if len(run_ids) == 0:
             return []
         # run_numbers are bigint, so cast the conditions array to the correct type.
         run_id_cond = "run_number = ANY (array[%s]::bigint[])"
@@ -98,7 +104,7 @@ class ArtifactCacheStore(object):
 
         # be sure to return a list of unique locations
         return list(frozenset(artifact['location'] for artifact in _records.body if 'location' in artifact))
-    
+
     async def get_run_parameters(self, flow_name, run_number):
         '''Fetches run parameter artifact locations,
         fetches the artifact content from S3, parses it, and returns
@@ -115,7 +121,7 @@ class ArtifactCacheStore(object):
         OR None
         '''
         run_id_key, run_id_value = translate_run_key(run_number)
-      
+
         # '_parameters' step has all the parameters as artifacts. only pick the
         # public parameters (no underscore prefix)
         db_response, _ = await self._artifact_table.find_records(
@@ -131,16 +137,16 @@ class ArtifactCacheStore(object):
                 run_id_value,
                 "_parameters",
                 "\_%",
-                "name" #exclude the 'name' parameter as this always exists, and contains the FlowName
+                "name"  # exclude the 'name' parameter as this always exists, and contains the FlowName
             ],
             fetch_single=False
         )
         # Return nothing if params artifacts were not found.
         if not db_response.response_code == 200:
             return None
-            
+
         # Fetch the values for the given parameters through the cache client.
-        locations = [ art["location"] for art in db_response.body if "location" in art ]
+        locations = [art["location"] for art in db_response.body if "location" in art]
         _params = await self.cache.GetArtifacts(locations)
         if not _params.is_ready():
             async for event in _params.stream():
@@ -157,9 +163,9 @@ class ArtifactCacheStore(object):
                 combined_results[key] = {
                     "value": _params[art["location"]]
                 }
-        
+
         return combined_results
-    
+
     def run_parameters_event_handler(self, *args, **kwargs):
         asyncio.run_coroutine_threadsafe(self._run_parameters_event_handler(*args, **kwargs), self.loop)
 
@@ -172,7 +178,7 @@ class ArtifactCacheStore(object):
                 [f"/flows/{flow_id}/runs/{run_number}/parameters"],
                 parameters
             )
-    
+
     def preload_event_handler(self, run_id):
         "Handler for event-emitter for preloading artifacts for a run id"
         asyncio.run_coroutine_threadsafe(self.preload_data_for_runs([run_id]), self.loop)
@@ -181,7 +187,6 @@ class ArtifactCacheStore(object):
         await self.cache.stop()
 
 
-from .generate_dag_action import GenerateDag
 class DAGCacheStore(object):
     def __init__(self):
         self._run_table = AsyncPostgresDB.get_instance().run_table_postgres
@@ -190,12 +195,13 @@ class DAGCacheStore(object):
     async def start_cache(self):
         actions = [GenerateDag]
         self.cache = CacheAsyncClient('cache_data/dag',
-                                    actions,
-                                    max_size=100000)
+                                      actions,
+                                      max_size=100000)
         await self.cache.start()
-    
+
     async def stop_cache(self):
         await self.cache.stop()
+
 
 class GetParametersFailed(Exception):
     def __init__(self, msg="Failed to Get Parameters", id="failed-to-get-parameters"):
