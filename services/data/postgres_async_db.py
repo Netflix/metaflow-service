3e0177b3ff9cb81604017eba65a4755414eeb233
Sakari Ikonen <64256562+saikonen@users.noreply.github.com>
2021-02-04 18:28:27 +0200

    Fix finished at timestamp for failed attempts (#108)
    * change task finished_at to use latest heartbeat if no other timestamp is available.

--- services/data/postgres_async_db.py
+++ services/data/postgres_async_db.py
@@ -885,7 +885,13 @@ class AsyncTaskTablePostgres(AsyncPostgresTable):
     select_columns = ["tasks_v3.{0} AS {0}".format(k) for k in keys]
     join_columns = [
         "attempt.started_at as started_at",
-        "attempt.finished_at as finished_at",
+        """
+        (CASE
+        WHEN attempt.finished_at IS NULL AND {table_name}.last_heartbeat_ts IS NOT NULL
+        THEN {table_name}.last_heartbeat_ts*1000
+        ELSE attempt.finished_at
+        END) as finished_at
+        """.format(table_name=table_name),
         "attempt.attempt_ok as attempt_ok",
         # If 'attempt_ok' is present, we can leave task_ok NULL since
         #   that is used to fetch the artifact value from remote location.
