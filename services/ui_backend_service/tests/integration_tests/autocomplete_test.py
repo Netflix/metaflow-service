ad165d714e6f457371e72f55259b11ce386ce19b
Sakari Ikonen <sakari.ikonen@codemate.com>
2021-03-25 12:47:25 +0200

    improve autocomplete integration test coverage.

--- services/ui_backend_service/tests/integration_tests/autocomplete_test.py
+++ services/ui_backend_service/tests/integration_tests/autocomplete_test.py
@@ -32,14 +32,28 @@ async def test_runs_autocomplete(cli, db):
     await _test_list_resources(cli, db, '/flows/HelloFlow/runs/autocomplete', 200, [])
     await add_flow(db, flow_id="HelloFlow")
     await add_run(db, flow_id="HelloFlow", run_id="HelloRun")
+
     await _test_list_resources(cli, db, '/flows/HelloFlow/runs/autocomplete', 200, ['HelloRun'])
+
+    # Partial matches
+    await _test_list_resources(cli, db, '/flows/HelloFlow/runs/autocomplete?run:co=llo', 200, ['HelloRun'])
+
+    # no-match
+    await _test_list_resources(cli, db, '/flows/HelloFlow/runs/autocomplete?run:co=test', 200, [])
 
 
 async def test_steps_autocomplete(cli, db):
     _flow = (await add_flow(db, flow_id="HelloFlow")).body
     _run = (await add_run(db, flow_id=_flow.get("flow_id"))).body
-    _step = (await add_step(db, flow_id=_run.get("flow_id"), step_name="end", run_number=_run.get("run_number"), run_id=_run.get("run_id"))).body
-    await _test_list_resources(cli, db, '/flows/{flowid}/runs/{runid}/steps/autocomplete'.format(flowid=_step.get('flow_id'), runid=_step.get('run_number')), 200, [_step.get('step_name')])
+    _step = (await add_step(db, flow_id=_run.get("flow_id"), step_name="regular_step", run_number=_run.get("run_number"), run_id=_run.get("run_id"))).body
+    
+    await _test_list_resources(cli, db, '/flows/{flowid}/runs/{runid}/steps/autocomplete'.format(flowid=_step.get('flow_id'), runid=_step.get('run_number')), 200, ["regular_step"])
+    
+    # Partial match
+    await _test_list_resources(cli, db, '/flows/{flowid}/runs/{runid}/steps/autocomplete?step_name:co=lar'.format(flowid=_step.get('flow_id'), runid=_step.get('run_number')), 200, ["regular_step"])
+
+    # no-match
+    await _test_list_resources(cli, db, '/flows/{flowid}/runs/{runid}/steps/autocomplete?step_name:co=test'.format(flowid=_step.get('flow_id'), runid=_step.get('run_number')), 200, [])
 
 
 async def test_tags_autocomplete(cli, db):
@@ -48,3 +62,12 @@ async def test_tags_autocomplete(cli, db):
     await add_run(db, flow_id="HelloFlow", run_id="HelloRun", tags=["tag:something"])
     # Note that runtime:dev tags gets assigned automatically
     await _test_list_resources(cli, db, '/tags/autocomplete', 200, ['runtime:dev', 'tag:something'])
+    
+    # Partial match
+    await _test_list_resources(cli, db, '/tags/autocomplete?tag:co=thing', 200, ['tag:something'])
+
+    # no-match
+    await _test_list_resources(cli, db, '/tags/autocomplete?tag:co=nothing', 200, [])
+
+    # Custom match
+    await _test_list_resources(cli, db, '/tags/autocomplete?tag:li=tag:%25thing', 200, ['tag:something'])
