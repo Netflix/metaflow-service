eac804da4ad0d4a42ef32a10f0a4ce764a2d32fd
Sakari Ikonen <sakari.ikonen@codemate.com>
2021-03-26 15:02:25 +0200

    feat: running tests with docker-compose and configure codestyles     * add Tox support and pycodestyle directives     * update .gitignore for tox temp files     * add docker-compose and dockerfile for running tests     * add placeholder test for metadata_service     * pin service dependency versions

--- setup.py
+++ setup.py
@@ -1,32 +1,53 @@
+from os.path import dirname, join, exists
 from setuptools import setup, find_packages
+
+
+def open_and_read_if_exists(path: str):
+    try:
+        with open(join(dirname(__file__), path)) as f:
+            return f.read()
+    except:
+        return ""
+
+
+requirements = []
+for service in ['metadata_service', 'migration_service']:
+    requirements += open_and_read_if_exists(
+        "services/{}/requirements.txt".format(service)).splitlines()
+
+requirements_tests = open_and_read_if_exists(
+    'requirements.dev.txt').splitlines()
+
+long_description = open_and_read_if_exists(
+    'README.md')
+
 setup(
-  name = 'metadata_service',
-  version = '2.0.4',
-  license='Apache License 2.0',
-  description = 'Metadata Service: backend service for Metaflow',
-  author = 'Machine Learning Infrastructure Team at Netflix',
-  author_email = 'help@metaflow.org',
-  url = 'https://github.com/Netflix/metaflow-service',
-  keywords = ['metaflow', 'machinelearning', 'ml'],
-  py_modules=['metadata_service'],
-  packages=find_packages(),
-  entry_points='''
+    name='metadata_service',
+    version='2.0.4',
+    license='Apache License 2.0',
+    description='Metadata Service: backend service for Metaflow',
+    long_description=long_description,
+    author='Machine Learning Infrastructure Team at Netflix',
+    author_email='help@metaflow.org',
+    url='https://github.com/Netflix/metaflow-service',
+    keywords=['metaflow', 'machinelearning', 'ml'],
+    py_modules=['services.metadata_service'],
+    packages=find_packages(exclude=('tests',)),
+    entry_points='''
         [console_scripts]
         metadata_service=services.metadata_service.server:main
+        migration_service=services.migration_service.migration_server:main
    ''',
-  install_requires=[
-          'aiohttp',
-          'aiohttp_swagger',
-          'psycopg2',
-          'aiopg',
-          'boto3',
-
-      ],
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: Apache Software License',
-    'Programming Language :: Python :: 3.7',
-  ],
+    install_requires=requirements,
+    tests_require=requirements + requirements_tests,
+    extras_require={
+        'test': requirements + requirements_tests
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
