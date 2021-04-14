51e83656cf41d70c2fe212e7c7e9dc94f0cfb0fb
Sakari Ikonen <sakari.ikonen@codemate.com>
2021-04-14 13:09:55 +0300

    fix: cast service config env_vars to correct types.

--- services/data/service_configs.py
+++ services/data/service_configs.py
@@ -1,6 +1,6 @@
 import os
 
-max_connection_retires = os.environ.get("MF_SERVICE_CONNECTION_RETRIES", 3)
-connection_retry_wait_time_seconds = os.environ.get("MF_SERVICE_CONNECTION_RETRY_WAITTIME_SECONDS", 1)
-max_startup_retries = os.environ.get("MF_SERVICE_STARTUP_RETRIES", 5)
-startup_retry_wait_time_seconds = os.environ.get("MF_SERVICE_STARTUP_WAITTIME_SECONDS", 1)
+max_connection_retires = int(os.environ.get("MF_SERVICE_CONNECTION_RETRIES", 3))
+connection_retry_wait_time_seconds = int(os.environ.get("MF_SERVICE_CONNECTION_RETRY_WAITTIME_SECONDS", 1))
+max_startup_retries = int(os.environ.get("MF_SERVICE_STARTUP_RETRIES", 5))
+startup_retry_wait_time_seconds = int(os.environ.get("MF_SERVICE_STARTUP_WAITTIME_SECONDS", 1))
