ed48ed1954dc47509746161710e5f35737db7247
Sakari Ikonen <sakari.ikonen@codemate.com>
2021-04-27 16:16:20 +0300

    feat: handle empty search term with artifact search efficiently. Clean up code.

--- services/ui_backend_service/api/artifactsearch.py
+++ services/ui_backend_service/api/artifactsearch.py
@@ -18,41 +18,47 @@ class ArtifactSearchApi(object):
     @handle_exceptions
     async def get_run_tasks(self, request):
         flow_name = request.match_info['flow_id']
-        run_id_key, run_id_value = translate_run_key(
-            request.match_info['run_number'])
+        run_key = request.match_info['run_number']
         artifact_name = request.query['key']
-        value = request.query['value']
+        value = request.query.get('value', None)
 
-        meta_artifacts = await self.get_run_artifacts(flow_name, run_id_key, run_id_value, artifact_name)
+        meta_artifacts = await self.get_run_artifacts(flow_name, run_key, artifact_name)
 
         ws = web.WebSocketResponse()
         await ws.prepare(request)
 
-        # Search the artifact contents from S3 using the CacheClient
-        locations = [art['location'] for art in meta_artifacts]
-        res = await self._artifact_store.cache.SearchArtifacts(locations, value)
-
-        if res.is_ready():
-            artifact_data = res.get()
+        if value is None:
+            # For empty search terms simply return the list of tasks that have artifacts by the specified name.
+            # Do not unnecessarily hit the cache.
+            results = [{**_result_format(art), "searchable": True} for art in meta_artifacts]
         else:
-            async for event in res.stream():
-                await ws.send_str(json.dumps(event))
-                if event["event"]["type"] == "error":
-                    # close websocket if an error is encountered.
-                    await ws.close(code=1011)
-            await res.wait()
-            artifact_data = res.get()
+            # Search through the artifact contents from S3 using the CacheClient
+            locations = [art['location'] for art in meta_artifacts]
+            res = await self._artifact_store.cache.SearchArtifacts(locations, value)
 
-        results = await search_dict_filter(meta_artifacts, artifact_data)
+            if res.is_ready():
+                artifact_data = res.get()
+            else:
+                async for event in res.stream():
+                    await ws.send_str(json.dumps(event))
+                    if event["event"]["type"] == "error":
+                        # close websocket if an error is encountered.
+                        await ws.close(code=1011)
+                await res.wait()
+                artifact_data = res.get()
+
+            results = await _search_dict_filter(meta_artifacts, artifact_data)
 
         await ws.send_str(json.dumps({"event": {"type": "result", "matches": results}}))
 
         return ws
 
-    async def get_run_artifacts(self, flow_name, run_id_key, run_id_value, artifact_name):
-        '''find a set of artifacts to perform the search over.
+    async def get_run_artifacts(self, flow_name, run_key, artifact_name):
+        """
+        Find a set of artifacts to perform the search over.
         Includes localstore artifacts as well, as we want to return that these could not be searched over.
-        '''
+        """
+        run_id_key, run_id_value = translate_run_key(run_key)
         meta_artifacts = await self._artifact_table.get_records(
             filter_dict={
                 "flow_id": flow_name,
@@ -65,36 +71,39 @@ class ArtifactSearchApi(object):
 # Utilities
 
 
-async def search_dict_filter(artifacts, artifact_match_dict={}):
-    '''Returns artifacts that match the searchterm with their content.
+async def _search_dict_filter(artifacts, artifact_match_dict={}):
+    """
+    Combines search match data dict with a list of artifacts to create actual search results.
 
-    Requirements:
+    Parameters
+    ----------
+    artifacts: List
+        list of artifacts that have a location key.
+        [{..., 'location': 'a_location'}]
 
-    artifacts: [{..., 'location': 'a_location'}]
+    artifact_match_dict: Dict
+        dictionary of location-based match data.
+        example:
+        {'a_location': {'matches': boolean, 'included': boolean}}
+        Matches: whether the search term matched the artifact content or not
+        Included: Whether the artifact content was included in the search or not (was the content accessible at all)
 
-    artifact_match_dict: {'a_location': {'matches': boolean, 'included': boolean}}
-      Matches: whether the search term matched the artifact content or not
-      Included: Whether the artifact content was included in the search or not (was the content accessible at all)
-
-    Returns:
-    [
-        {
-            'flow_id': str,
-            'run_number': int,
-            'step_name': str,
-            'task_id': int,
-            'searchable': boolean
-        }
-    ]
-      searchable: denotes whether the task had an artifact that could be searched or not.
-      False in cases where the artifact could not be included in the search
-    '''
-
-    def result_format(art):
-        return dict(
-            [key, val] for key, val in art.items()
-            if key in ['flow_id', 'run_number', 'step_name', 'task_id']
-        )
+    Returns
+    -------
+    List
+        example:
+        [
+            {
+                'flow_id': str,
+                'run_number': int,
+                'step_name': str,
+                'task_id': int,
+                'searchable': boolean
+            }
+        ]
+        searchable: denotes whether the task had an artifact that could be searched or not.
+        False in cases where the artifact could not be included in the search
+    """
 
     results = []
     for artifact in artifacts:
@@ -102,6 +111,13 @@ async def search_dict_filter(artifacts, artifact_match_dict={}):
         if loc in artifact_match_dict:
             match_data = artifact_match_dict[loc]
             if match_data['matches'] or not match_data['included']:
-                results.append({**result_format(artifact), "searchable": match_data['included']})
+                results.append({**_result_format(artifact), "searchable": match_data['included']})
 
     return results
+
+
+def _result_format(art):
+    return dict(
+        [key, val] for key, val in art.items()
+        if key in ['flow_id', 'run_number', 'step_name', 'task_id']
+    )
