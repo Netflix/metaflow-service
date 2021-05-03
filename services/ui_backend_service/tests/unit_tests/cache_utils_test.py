ca9e5a1bc16f3d44d7ab214d4b376ae5d87ca5a5
Sakari Ikonen <sakari.ikonen@codemate.com>
2021-05-03 16:21:10 +0300

    add unit tests for cache action stream msg formatters

---
+++ services/ui_backend_service/tests/unit_tests/cache_utils_test.py
@@ -0,0 +1,19 @@
+import pytest
+
+from services.ui_backend_service.data.cache.utils import (
+    error_event_msg, progress_event_msg, search_result_event_msg
+)
+
+pytestmark = [pytest.mark.unit_tests]
+
+
+def test_error_event_msg():
+  assert error_event_msg("test message", "test-id") == {"type": "error", "message": "test message", "id": "test-id"}
+
+
+def test_progress_event_msg():
+  assert progress_event_msg(0.5) == {"type": "progress", "fraction": 0.5}
+
+
+def test_search_result_event_msg():
+  assert search_result_event_msg([1, 2, 3]) == {"type": "result", "matches": [1, 2, 3]}
