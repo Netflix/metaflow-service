4c77e30ccfe13d81e309ae5fe2f1586cfa6a1404
Matias Savela <matias.savela@codemate.com>
2021-10-01 12:22:50 +0300

    refactor[cache]: Prepare Cache Worker Metaflow Client configuration variables, create new GetData action and prepare run/task table changes for refinement purposes.

---
+++ services/ui_backend_service/data/cache/get_data_action.py
@@ -0,0 +1,151 @@
+import json
+import hashlib
+
+from .client import CacheAction
+from services.utils import get_traceback_str
+
+from metaflow import namespace
+
+namespace(None)  # Always use global namespace by default
+
+
+class GetData(CacheAction):
+    """
+    Fetches data by target list and returning their contents.
+
+    Parameters
+    ----------
+    targets : List[str]
+        A list of targets to fetch, usually pathspecs to be used with Metaflow Client API.
+    invalidate_cache: bool
+        Force cache invalidation
+
+    Returns
+    -------
+    Dict or None
+        example:
+        {
+            "targets": ["FlowId/RunNumber"]
+        }
+    """
+
+    @classmethod
+    def format_request(cls, *args, **kwargs):
+        targets = kwargs.get('targets', [])
+        invalidate_cache = kwargs.get('invalidate_cache', False)
+
+        msg = {
+            'targets': list(frozenset(sorted(targets)))
+        }
+
+        target_keys = []
+        for object in targets:
+            target_keys.append(cache_key_from_target(object, "data:{}:".format(cls.__name__)))
+
+        request_id = lookup_id(targets)
+        stream_key = cache_key_from_target(request_id, "data:stream:{}:".format(cls.__name__))
+
+        return msg,\
+            target_keys,\
+            stream_key,\
+            [stream_key],\
+            invalidate_cache
+
+    @classmethod
+    def response(cls, keys_objs):
+        '''Action should respond with a dictionary of
+        {
+            "FlowId/RunNumber": [boolean, "contents"]
+        }
+        '''
+
+        target_keys = [(key, val) for key, val in keys_objs.items() if key.startswith("data:{}:".format(cls.__name__))]
+
+        collected = {}
+        for key, val in target_keys:
+            collected[target_from_cache_key(key, "data:{}:".format(cls.__name__))] = json.loads(val)
+
+        return collected
+
+    @classmethod
+    def stream_response(cls, it):
+        for msg in it:
+            yield msg
+
+    @classmethod
+    def execute(cls,
+                message=None,
+                keys=None,
+                existing_keys={},
+                stream_output=None,
+                invalidate_cache=False,
+                **kwargs):
+        """
+        Execute fetching of targets.
+        Produces cache results that are either successful or failed.
+
+        Success cached example:
+            results[target_key] = json.dumps([True, str(content)])
+        Failure cached example:
+            results[target_key] = json.dumps([False, 'artifact-too-large', artifact_data.size])
+
+        Alternatively errors can be streamed to cache client using `stream_error`.
+        This way failures won't be cached for individual artifacts, thus making
+        it necessary to retry fetching during next attempt. (Will add significant overhead/delay).
+
+        Streaming errors should be avoided to minimize latency for subsequent requests.
+
+        Stream error example:
+            stream_error(str(ex), "s3-not-found", get_traceback_str(), artifact_key)
+        """
+        targets = message['targets']
+
+        if invalidate_cache:
+            results = {}
+            targets_to_fetch = [loc for loc in targets]
+        else:
+            # make a copy of already existing results, as the cache action has to produce all keys it promised
+            # in the format_request response.
+            results = {**existing_keys}
+            # Make a list of artifact locations that require fetching (not cached previously)
+            targets_to_fetch = [loc for loc in targets if not cache_key_from_target(loc, "data:{}:".format(cls.__name__)) in existing_keys]
+
+        for target in targets_to_fetch:
+            target_key = cache_key_from_target(target, "data:{}:".format(cls.__name__))
+            try:
+                results[target_key] = json.dumps(cls.fetch_data(target))
+            except Exception as ex:
+                results[target_key] = json.dumps([False, 'data-not-accessible', get_traceback_str()])
+
+        return results
+
+    @classmethod
+    def fetch_data(cls, *args, **kwargs):
+        """
+        Decodes and refines `execute` output before it is returned
+        to the client. The argument `keys_objs` is the return value
+        of `execute`. This method is called by `cache_client` to
+        convert serialized, cached results to a client-facing object.
+
+        The function may return anything.
+        """
+        raise NotImplementedError
+
+
+DATA_CACHE_ID_PREFIX = 'data:'
+
+
+def cache_key_from_target(target, prefix=DATA_CACHE_ID_PREFIX):
+    "construct a unique cache key for target"
+    return "{}{}".format(prefix, target)
+
+
+def target_from_cache_key(cache_key, prefix=DATA_CACHE_ID_PREFIX):
+    "extract location from the target cache key"
+    return cache_key[len(prefix):]
+
+
+def lookup_id(targets):
+    "construct a unique id to be used with stream_key and result_key"
+    _string = "-".join(list(frozenset(sorted(targets))))
+    return hashlib.sha1(_string.encode('utf-8')).hexdigest()
