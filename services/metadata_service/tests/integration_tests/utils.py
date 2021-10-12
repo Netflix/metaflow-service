2c5071b5a92c613b9fcf89de40b1662f97fb6dd5
Sakari Ikonen <sakari.ikonen@codemate.com>
2021-10-12 20:35:51 +0300

    fix db_conf dsn related issues after pulling latest changes from master. Update unit tests and test-suite env guard

--- services/metadata_service/tests/integration_tests/utils.py
+++ services/metadata_service/tests/integration_tests/utils.py
@@ -26,10 +26,10 @@ def get_test_dbconf():
     "Returns a DBConfiguration suitable for the test environment, or exits pytest completely upon failure"
     db_conf = DBConfiguration()
 
-    if db_conf.dsn != "dbname=test user=test password=test host=db_test port=5432":
+    if db_conf.dsn != "dbname=test user=test host=db_test port=5432 password=test":
         pytest.exit("The test suite should only be run in a test environment. \n \
             Configured database host is not suited for running tests. \n \
-            expected DSN to be: dbname=test user=test password=test host=db_test port=5432")
+            expected DSN to be: dbname=test user=test host=db_test port=5432 password=test")
 
     return db_conf
 
