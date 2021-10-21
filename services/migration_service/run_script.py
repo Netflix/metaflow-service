09b47fefd8437c823292af8cd8743438773aa652
Matias Savela <matias.savela@codemate.com>
2021-10-21 12:27:27 +0300

    Provide 'latest' virtualenv for migration service in order to request metadata_service virtualenv version

--- services/migration_service/run_script.py
+++ services/migration_service/run_script.py
@@ -1,13 +1,23 @@
 from subprocess import Popen
 import os
+
+
+def setup_env(version_value: str):
+    _env = os.environ
+    virtual_env_path = '/opt/' + version_value
+    _env['VIRTUAL_ENV'] = virtual_env_path
+    path = _env['PATH']
+    _env['PATH'] = virtual_env_path + "/bin:" + path
+    return _env
+
+
 if __name__ == "__main__":
     try:
-        my_env = os.environ
         migration_server_process = Popen(
             "PYTHONPATH=/ python3 -m services.migration_service.migration_server",
             shell=True,
             close_fds=True,
-            env=my_env
+            env=setup_env('latest')
         )
 
         get_env_version = Popen(
@@ -23,15 +33,11 @@ if __name__ == "__main__":
         version_value = str(version_value_file.read()).strip()
 
         # start proper version of metadata service
-        virtual_env_path = '/opt/' + version_value
-        my_env['VIRTUAL_ENV'] = '/opt/' + version_value
-        path = my_env['PATH']
-        my_env['PATH'] = virtual_env_path + "/bin:" + path
         metadata_server_process = Popen(
             "metadata_service",
             shell=True,
             close_fds=True,
-            env=my_env
+            env=setup_env(version_value)
         )
 
         metadata_server_process.wait()
