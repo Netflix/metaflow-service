b3a530ee6f69c782db23f088dc7c55b5e9949bc3
Oleg Avdeev <oleg.v.avdeev@gmail.com>
2022-02-16 18:27:54 -0800

    add a standalone script to run goose migrations (used in helm charts)

---
+++ run_goose.py
@@ -0,0 +1,25 @@
+from subprocess import Popen
+import os
+from urllib.parse import quote
+
+if __name__ == "__main__":
+    db_connection_string = "postgresql://{}:{}@{}:{}/{}?sslmode=disable".format(
+        quote(os.environ["MF_METADATA_DB_USER"]),
+        quote(os.environ["MF_METADATA_DB_PSWD"]),
+        os.environ["MF_METADATA_DB_HOST"],
+        os.environ["MF_METADATA_DB_PORT"],
+        os.environ["MF_METADATA_DB_NAME"],
+    )
+
+    p = Popen(
+        [
+            "/go/bin/goose",
+            "-dir",
+            "/root/services/migration_service/migration_files/",
+            "postgres",
+            db_connection_string,
+            "up",
+        ]
+    )
+    if p.wait() != 0:
+        raise Exception("Failed to run initial migration")
